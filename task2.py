c8d756f8ab19aace1ff6ebb589f8d713f30d2e79
Alena-Ilova <alena.i@solargroup.team>
2024-11-13 19:11:29 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,14 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def is_power(n, base):
+    if base <= 1:
+          return n == 1
+
+    while n > 1:
+        if n % base != 0:
+            return False
+        n //= base 
+    return n == 1
+
+print(is_power(16, 2))
+print(is_power(12, 2))
+print(is_power(100000, 10))
+print(is_power(990, 9))
